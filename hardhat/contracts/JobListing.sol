407df0554ae78e03b3155c711d501cf6d55c3864
Gabriel Mania <mania.gabb@gmail.com>
2023-04-26 15:11:17 +0800

    added comments on JobListing.sol

--- hardhat/contracts/JobListing.sol
+++ hardhat/contracts/JobListing.sol
@@ -2,20 +2,25 @@
 pragma solidity ^0.8.0;
 
 contract JobListing {
+    // Struct to store data about a job listing
     struct Job {
-        uint id;
-        address employer;
-        string title;
-        string description;
-        string requiredSkills;
-        uint experience;
-        uint compensation;
-        bool isActive;
+        uint id; // Unique identifier for the job listing
+        address employer; // Address of the employer who posted the job
+        string title; // Job title
+        string description; // Job description
+        string requiredSkills; // Required skills for the job
+        uint experience; // Required experience level
+        uint compensation; // Compensation offered for the job
+        bool isActive; // Status of the job: true if active, false otherwise
     }
 
+    // Counter to keep track of the total number of job listings
     uint private jobCounter;
+
+    // Mapping to store job listings by their unique identifier
     mapping(uint => Job) private jobs;
 
+    // Event emitted when a new job listing is posted
     event JobPosted(
         uint indexed jobId,
         address indexed employer,
@@ -25,6 +30,8 @@ contract JobListing {
         uint experience,
         uint compensation
     );
+
+    // Event emitted when a job listing is updated
     event JobUpdated(
         uint indexed jobId,
         string title,
@@ -33,8 +40,11 @@ contract JobListing {
         uint experience,
         uint compensation
     );
+
+    // Event emitted when a job listing is removed
     event JobRemoved(uint indexed jobId);
 
+    // Function to post a new job listing
     function postJob(
         string memory _title,
         string memory _description,
@@ -44,6 +54,7 @@ contract JobListing {
     ) public {
         jobCounter++;
 
+        // Create a new Job struct and store it in the jobs mapping
         Job memory newJob = Job(
             jobCounter,
             msg.sender,
@@ -56,6 +67,7 @@ contract JobListing {
         );
         jobs[jobCounter] = newJob;
 
+        // Emit the JobPosted event
         emit JobPosted(
             jobCounter,
             msg.sender,
@@ -67,6 +79,7 @@ contract JobListing {
         );
     }
 
+    // Function to update an existing job listing
     function updateJob(
         uint _jobId,
         string memory _title,
@@ -77,18 +90,22 @@ contract JobListing {
     ) public {
         Job storage job = jobs[_jobId];
 
+        // Ensure that only the employer can update the job listing
         require(
             job.employer == msg.sender,
             "Only the job employer can update the job"
         );
+        // Ensure that the job listing is still active
         require(job.isActive, "Job is not active");
 
+        // Update the job details
         job.title = _title;
         job.description = _description;
         job.requiredSkills = _requiredSkills;
         job.experience = _experience;
         job.compensation = _compensation;
 
+        // Emit the JobUpdated event
         emit JobUpdated(
             _jobId,
             _title,
@@ -99,25 +116,32 @@ contract JobListing {
         );
     }
 
+    // Function to remove a job listing
     function removeJob(uint _jobId) public {
         Job storage job = jobs[_jobId];
 
+        // Ensure that only the employer can remove the job listing
         require(
             job.employer == msg.sender,
             "Only the job employer can remove the job"
         );
+        // Ensure that the job listing is still active
         require(job.isActive, "Job is not active");
-
+        // Set the job listing status to inactive
         job.isActive = false;
 
+        // Emit the JobRemoved event
         emit JobRemoved(_jobId);
     }
 
+    // Function to retrieve a job listing by its ID
     function getJob(uint _jobId) public view returns (Job memory) {
         return jobs[_jobId];
     }
 
+    // Function to retrieve all active job listings
     function getAllJobs() public view returns (Job[] memory) {
+        // Count the number of active jobs
         uint activeJobsCount = 0;
         for (uint i = 1; i <= jobCounter; i++) {
             if (jobs[i].isActive) {
@@ -125,7 +149,10 @@ contract JobListing {
             }
         }
 
+        // Create an array to store the active Job structs
         Job[] memory allJobs = new Job[](activeJobsCount);
+
+        // Iterate through the jobs mapping and add active jobs to the array
         uint index = 0;
         for (uint i = 1; i <= jobCounter; i++) {
             if (jobs[i].isActive) {
@@ -134,6 +161,7 @@ contract JobListing {
             }
         }
 
+        // Return the array of active Job structs
         return allJobs;
     }
 }
