2d1dcb9ea82070d7e1e9c90a78c6df65d4ee8bca
Gabriel Mania <mania.gabb@gmail.com>
2023-04-26 14:03:26 +0800

    added comments on the DisputeResolution.sol

--- hardhat/contracts/DisputeResolution.sol
+++ hardhat/contracts/DisputeResolution.sol
@@ -2,12 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract DisputeResolution {
+    // 'DisputeStatus' stores the possible status of the dispute
     enum DisputeStatus {
         Created,
         Resolved,
         Rejected
     }
 
+    // 'Dispute' contains the details for the dispute
     struct Dispute {
         uint id;
         uint jobId;
@@ -18,9 +20,12 @@ contract DisputeResolution {
         uint256 timestamp;
     }
 
+    // 'disputeCounter' provides a counter for automatically generating an ID
     uint private disputeCounter;
+    // 'disputes' mapping of dispute ID to the 'Dispute' struct
     mapping(uint => Dispute) private disputes;
 
+    // events for dispute creation, resolution, and rejection
     event DisputeCreated(
         uint indexed disputeId,
         uint indexed jobId,
@@ -32,12 +37,17 @@ contract DisputeResolution {
     event DisputeResolved(uint indexed disputeId);
     event DisputeRejected(uint indexed disputeId);
 
+    // @dev 'createDispute' instantiate a new 'Dispute' struct and map that particular
+    //      dispute to the 'disputeCounter' then adds it to the 'dispute' mapping
+    // @params '_jobId' the ID of the job posting; for reference
+    //         '_respondent' the address of the employer or candidate
+    //         '_reason' the reason for the complaint
     function createDispute(
         uint _jobId,
         address _respondent,
         string memory _reason
     ) public {
-        disputeCounter++;
+        disputeCounter++; 
 
         Dispute memory newDispute = Dispute(
             disputeCounter,
@@ -60,32 +70,47 @@ contract DisputeResolution {
         );
     }
 
+    // @dev 'resolveDispute' resolves the dispute by based on the passed ID argument.
+    //      If dispute is already resolved or rejected, the transaction will be
+    //      reverted
+    // @params '_disputeId' ID of the dispute for proper referencing on the 'disputes'
+    //         mapping
     function resolveDispute(uint _disputeId) public {
         Dispute storage dispute = disputes[_disputeId];
 
+        // check if dispute status is 'Created'. if not, revert the transaction
         require(
             dispute.status == DisputeStatus.Created,
             "Dispute already resolved or rejected"
         );
 
+        // change the dispute status to 'Resolved'
         dispute.status = DisputeStatus.Resolved;
 
         emit DisputeResolved(_disputeId);
     }
 
+    // @dev 'rejectDispute' rejects the dispute based on the '_disputeId' passed
+    //      as an argument to the function
+    // @params '_disputeId' ID of the dispute for proper referencing on the 'disputes'
+    //         mapping
     function rejectDispute(uint _disputeId) public {
         Dispute storage dispute = disputes[_disputeId];
 
+        // check if dispute status is 'Created'. if not, revert the transaction
         require(
             dispute.status == DisputeStatus.Created,
             "Dispute already resolved or rejected"
         );
 
+        // change the dispute status to 'Rejected'
         dispute.status = DisputeStatus.Rejected;
 
         emit DisputeRejected(_disputeId);
     }
 
+    // 'getDispute' returns the 'Dispute' struct for the particular '_disputeId'
+    // passed as an argument
     function getDispute(uint _disputeId) public view returns (Dispute memory) {
         return disputes[_disputeId];
     }
