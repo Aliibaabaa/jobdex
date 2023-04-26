f8a4a920b4a694dbe91738734b0978584fb16dfc
Gabriel Mania <mania.gabb@gmail.com>
2023-04-26 11:46:33 +0800

    revised TokenManagement contract; added modifier onlyOwner

--- hardhat/contracts/TokenManagement.sol
+++ hardhat/contracts/TokenManagement.sol
@@ -2,9 +2,15 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 contract TokenManagement is ERC20 {
+    address public owner;
+
+    modifier onlyOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
     constructor(
         uint256 initialSupply
     ) ERC20("DecentralizedJobMarketToken", "DJMT") {
