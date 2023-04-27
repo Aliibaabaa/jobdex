019e505765f7d735237754d4a8d7e3e6f1bf5cd5
Gabriel Mania <mania.gabb@gmail.com>
2023-04-27 16:03:05 +0800

    revised TokenManagement.sol to interact with UserRegistry.sol

--- hardhat/contracts/TokenManagement.sol
+++ hardhat/contracts/TokenManagement.sol
@@ -3,11 +3,14 @@ pragma solidity ^0.8.0;
 
 // Import the ERC20 contract from OpenZeppelin
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./UserRegistry.sol";
 
 // Define the TokenManagement contract that inherits from the ERC20 contract
 contract TokenManagement is ERC20 {
     // Define the owner variable which represents the address of the contract owner
     address public owner; // Define a modifier called onlyOwner, which ensures that only the contract owner can call functions with this modifier
+    UserRegistry private userRegistry;
+
     modifier onlyOwner() {
         require(owner == msg.sender);
         _;
@@ -15,12 +18,15 @@ contract TokenManagement is ERC20 {
 
     // Define the constructor, which takes the initial token supply as an argument
     constructor(
-        uint256 initialSupply
+        uint256 initialSupply,
+        address _userRegistry
     ) ERC20("DecentralizedJobMarketToken", "DJMT") {
         // Mint the initial token supply and assign it to the contract deployer
         _mint(msg.sender, initialSupply);
         // Set the owner variable to the address of the contract deployer
         owner = msg.sender;
+
+        userRegistry = UserRegistry(_userRegistry);
     }
 
     // Define the mint function, which allows the contract owner to mint new tokens to a specified address
@@ -33,5 +39,28 @@ contract TokenManagement is ERC20 {
     function burn(address from, uint256 amount) public onlyOwner {
         // Call the internal _burn function to destroy tokens
         _burn(from, amount);
+    }
+
+    // Add a function to mint tokens by user ID
+    function mintByUserId(uint _userId, uint256 amount) public onlyOwner {
+        address to = userRegistry.getUserById(_userId).wallet;
+        _mint(to, amount);
+    }
+
+    // Add a function to burn tokens by user ID
+    function burnByUserId(uint _userId, uint256 amount) public onlyOwner {
+        address from = userRegistry.getUserById(_userId).wallet;
+        _burn(from, amount);
+    }
+
+    // Add a function to transfer tokens by user IDs
+    function transferByUserIds(
+        uint _fromUserId,
+        uint _toUserId,
+        uint256 amount
+    ) public onlyOwner {
+        address from = userRegistry.getUserById(_fromUserId).wallet;
+        address to = userRegistry.getUserById(_toUserId).wallet;
+        transferFrom(from, to, amount);
     }
 }
